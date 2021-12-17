be8afe1c5f82d352819f7735a78b30b1be67b32d
Mikhail Adlin <mikaadlin@hazyBook.local>
2021-12-17 09:55:11 -0500

    built grid, add css

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-const mainDiv = document.querySelector('.mainDiv');
+const mainDiv = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   let grid = document.createElement('div');
-  grid.classList.add("gridPiece");
-  grid.textContent = `Mika here`;
+  grid.classList.add('gridPiece');
   mainDiv.appendChild(grid);
+}
+
+
+
+const btn = document.querySelector('.btn');
+
+[...document.querySelectorAll('.gridPiece')].forEach((gridPiece) =>{
+  gridPiece.addEventListener('mouseover', function () {
+    gridPiece.style.backgroundColor = '#BAE4BF';
+  });
+  btn.addEventListener('click', () => {
+      clearPage();
+      newGrid ();
+    });
+})
+
+function clearPage() {
+  while (mainDiv.firstChild) {
+    mainDiv.removeChild(mainDiv.firstChild);
+  }
+}
+
+function newGrid() {
+  let k = prompt('How many squares per side?', '16');
+  let j = parseInt(k*k);
+  for (let i = 0; i < j; i++) {
+    console.log('ye');
+    let grid = document.createElement('div');
+    grid.classList.add('gridPiece');
+    mainDiv.appendChild(grid);
+  }
+
+  
 }
